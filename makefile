fb018f78cfb9054b2fc6e5968ce3f777ba456bcf
kipdoudou <442381788@qq.com>
2016-08-23 10:24:18 +0800

    可以用的，我自己的测试路由的

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CC = gcc
-OBJECTS = mr_test.o mr_common.o mr_timer.o
+OBJECTS = mr_test.o ../mr_common.o
 
 mrtest: $(OBJECTS)
 	$(CC) -o mrtest $(OBJECTS) -lpthread
@@ -7,11 +7,8 @@ mrtest: $(OBJECTS)
 mr_test.o: mr_test.c mr_test.h
 	$(CC) -c mr_test.c
 
-mr_commom.o: mr_common.c mr_common.h
-	$(CC) -c mr_common.c
-
-mr_timer.o: mr_timer.c mr_timer.h
-	$(CC) -c mr_timer.c
+mr_commom.o: ../mr_common.c ../mr_common.h
+	$(CC) -c ../mr_common.c
 
 clean :
-	rm $(OBJECTS)
+	rm $(OBJECTS) 
--- mr_test.c
+++ mr_test.c
@@ -1,9 +1,9 @@
-#include "mr_common.h"
+#include "../mr_common.h"
 #include "mr_test.h"
 
 
 static key_t mt_qkey = -1;
-static int   mt_qid = -1;
+static int   mt_qid = -1; 
 
 static mt_tshare_t  share = {
 	PTHREAD_MUTEX_INITIALIZER,
@@ -11,11 +11,12 @@ static mt_tshare_t  share = {
 };
 
 static pthread_t mrx_tid = -1;
+static int rqs[MAX_NODE_CNT];
+//???top.txt??
+static U8  top[MAX_NODE_CNT][MAX_NODE_CNT];
 
-int rqs[MAX_NODE_CNT];
-//此值从top.txt读入
-mtop_t top[MAX_NODE_CNT][MAX_NODE_CNT];
-fmd_t fmd[MAX_NODE_CNT];
+//deleted by test
+//fmd_t fmd[MAX_NODE_CNT];
 
 int main(int argc, char* argv[])
 {
@@ -23,7 +24,7 @@ int main(int argc, char* argv[])
 	int j;
 	int rval, stop;
 	void *result = NULL;
-
+	
 	EPT(stderr, "%s: main thread id = %ld\n", argv[0], pthread_self());
 //	EPT(stderr, "sizeof(MADR) = %d\n", sizeof(MADR));
 
@@ -43,7 +44,7 @@ int main(int argc, char* argv[])
 	}
 
 	/* initialize topology */
-	//参数是top.txt
+	//???参数是top.txt
 	rval = mt_tinit(argv[1]);
 	if (rval != 0) {
 		EPT(stderr, "mr_test: errors occur in topology initialization\n");
@@ -58,20 +59,20 @@ int main(int argc, char* argv[])
 		rval = 3;
 		goto process_return;
 	}
-
-	/* start timer */
+//deleted by test
+	/* start timer 
 	rval = mt_mac_timer();
-	memset(fmd, sizeof(fmd), 0);
+	memset(fmd, 0, sizeof(fmd));*/
 
 	stop = 0;
 	pthread_mutex_lock(&share.mutex);
 	while(0 == stop) {
-		pthread_cond_wait(&share.cond, &share.mutex);
+		pthread_cond_wait(&share.cond, &share.mutex); 
 
 		/* do something */
 	}
 //	EPT(stderr, "%s: certain thread quit\n", argv[0]);
-	pthread_mutex_unlock(&share.mutex);
+	pthread_mutex_unlock(&share.mutex); 
 
 process_return:
 	mt_queues_delete();
@@ -81,14 +82,14 @@ process_return:
 int mt_queues_init()
 {
 	int rval = 0;
-
+	
 	mt_qkey = ftok(PATH_CREATE_KEY, SN_MRTEST);
 	mt_qid = msgget(mt_qkey, IPC_CREAT|QUEUE_MODE);
 	if (mt_qid == -1) {
 		EPT(stderr, "mr_test: can not get queue\n");
 		rval = 1;
 	}
-
+	
 	return rval;
 }
 
@@ -105,8 +106,8 @@ void* mt_qrv_thread(void *arg)
 	int qid, rcnt;
 	mmsg_t rx_msg;
 	int rval, stop;
-
-	pthread_detach(pthread_self());
+	
+	pthread_detach(pthread_self());	
 
 	qid = *(int *)arg;
 	EPT(stderr, "mt_test: msg receiving thread id = %ld\n", pthread_self());
@@ -117,23 +118,20 @@ void* mt_qrv_thread(void *arg)
 		rval = 1;
 		goto thread_return;
 	}
-	srand(qid);
+//deleted by test
+//	srand(qid); 
 
 	rval = 0;
 	stop = 0;
-	while(0 == stop)
-	{
-		memset(&rx_msg, sizeof(rx_msg), 0);
+	while(0 == stop) {
+		memset(&rx_msg, 0, sizeof(rx_msg));
 //		EPT(stdout, "mr_test: reveive msg queue at qid %d\n", qid);
 		rcnt = msgrcv(qid, &rx_msg, MAX_DATA_LENGTH, 0, 0);
-		if (rcnt < 0)
-		{
-			if (EIDRM != errno)
-			{
+		if (rcnt < 0) {
+			if (EIDRM != errno) {
 				EPT(stderr, "mr_test: error in receiving msg, no:%d, meaning:%s\n", errno, strerror(errno));
-			}
-			else
-			{
+			} 
+			else {
 				EPT(stderr, "mr_test: quit msg receiving thread\n");
 			}
 			rval = 2;
@@ -141,8 +139,7 @@ void* mt_qrv_thread(void *arg)
 		}
 
 		rval = mt_rmsg_proc(&rx_msg, rcnt);
-		if (rval != 0)
-		{
+		if (rval != 0) {
 			/* report error */
 			EPT(stderr, "mr_test: error occurs, the node of rx_msg  = %d", rx_msg.node);
 		}
@@ -165,50 +162,53 @@ int mt_rmsg_proc(mmsg_t *msg, int cnt)
 	int j, rval = 0;
 
 	node = msg->node;
-	if (msg->mtype == MMSG_MT_RQID)
+	if (msg->mtype == MMSG_MT_RQID) 		//node : sa
 	{
-		qid = *(int*)msg->data;
+		qid = *(int*)msg->data;						//ask???
 
-		if (MR_ISUNI(node))
-		{
+		if (MR_ISUNI(node)) {
 			rqs[node-1] = qid;
 		}
-		else
-		{
+		else {
 			EPT(stderr, "mt_test: node error\n");
 			rval = 1;
 		}
 	}
-	else if (wh_rp_msg(msg->mtype))
+	else /* ²»Íù×ÔÉí·¢ËÍ£¬ÇÒÔÚÍØÆËµÄÔÊÐíÏÂ·¢ */		//from others sop packet
 	{
-		for (j = 0; j < MAX_NODE_CNT; j++)
+		for (j = 0; j < MAX_NODE_CNT; j++) 	
 		{
-			if (j == node-1 || top[node-1][j].link == 0)
+			if (j == node-1 || top[node-1][j] == 0)   /*if (j == node-1 || top[node-1][j].link == 0) */
 				continue;
-			if (rqs[j] == -1)
+			if (rqs[j] == -1) 
 			{
 				EPT(stderr, "mr_test: can not get the qid of rx node %d\n", j+1);
 				continue;
 			}
-			if (mt_getlp() <= top[node-1][j].relb)
-			{
-				rval = msgsnd(rqs[j], (void *)msg, cnt, 0);
-				EPT(stderr, "mt_test: msgsnd() write msg at qid %d of node %d\n", rqs[j], j+1);
-				if ( rval < 0 )
-				{
-					EPT(stderr, "mr_test: msgsnd() write msg failed,errno=%d[%s]\n", errno, strerror(errno));
-				}
+//deleted by test, delete the condition 
+//			if (mt_getlp() <= top[node-1][j].relb)			
+													//	srand(qid)	rand()/RAND_MAX
+			rval = msgsnd(rqs[j], (void *)msg, cnt, 0);	//只负责转发！！！
+			EPT(stderr, "mt_test: msgsnd() write msg at qid %d of node %d\n", rqs[j], j+1);
+			if ( rval < 0 ) 
+			{  
+				EPT(stderr, "mr_test: msgsnd() write msg failed,errno=%d[%s]\n", errno, strerror(errno));  
 			}
-			else
+//deleted by test, delete the condition 
+/*
+	else
 			{
 				EPT(stderr, "mt_test: msgsnd() do not write msg at qid %d of node %d due to link error\n", rqs[j], j+1);
 			}
+*/
 		}
 	}
+//deleted by test
+/*
 	else if (MMSG_MT_MQID == msg->mtype)
 	{
 		qid = *(int*)msg->data;
-
+		
 		if (MR_ISUNI(node))
 		{
 			rqs[node-1] = qid;
@@ -219,93 +219,72 @@ int mt_rmsg_proc(mmsg_t *msg, int cnt)
 			rval = 1;
 		}
 	}
-	else if (wh_mac_msg(msg->mtype))
+	else if (wh_mac_msg(msg->mtype))			//MMSG_MT_MDT
 	{
 		msg_copy(&fmd[node-1].msg, msg, cnt);
 		fmd[node-1].len = cnt;
 	}
+*/
 
 	return rval;
 }
 
-//把top.txt中的数据读入top数组
+//?把top.txt??????txt中的数据读入top数组??
 int mt_tinit(char *name)
 {
 	int i, j;
-	float p;
 	int rval = 0;
 	FILE *fp = NULL;
-    //a+方式打开，不清空源文件内容，只在末尾追加
+	//a+方式打开，不清空源文件内容，只在末尾追加
 	fp = fopen(name, "a+");
-	if (NULL == fp)
+	if (NULL == fp) 
 	{
 		rval = 1;
 		goto fexit;
 	}
 
 	/* clear */
-	for (i = 0; i < MAX_NODE_CNT; i++)
+	for (i = 0; i < MAX_NODE_CNT; i++) 
 	{
 		rqs[i] = -1;
 		for (j = 0; j < MAX_NODE_CNT; j++)
-		{
+			top[i][j] = 0;
+//deleted by test
+/*
 			top[i][j].link = 0;
 			top[i][j].relb = 0.0;
-		}
+*/
 	}
 
-	while(!feof(fp))
+	while(!feof(fp)) 	//?????????判断文件是否结束。feof（fp）用于测试fp所指向的文件的当前状态是否为“文件结束”。如果是，函数则返回的值是1（真），否则为0（假）(fp)????fp??????????????“????”????,????????1(?),???0(?)
 	{
-		fscanf(fp, "%d %d %f", &i, &j, &p);
+		fscanf(fp, "%d %d", &i, &j); //从流中执行格式化输入的函数. fscanf遇到空格和换行时结束,返回值：整型，数值等于[argument...]的个数
 		if (MR_ISUNI(i) && MR_ISUNI(j))
-		{
-			top[i-1][j-1].link = 1;
-			top[i-1][j-1].relb = p;
-		}
+			top[i-1][j-1] = 1;
 	}
 
-	show_top2();
+	mt_show_top();
 fexit:
 	if (NULL != fp)
 		fclose(fp);
 	return rval;
 }
 
-void show_top1()
-{
-	int i,j;
-	EPT(stderr, "   ");
-	for (i = 0; i < MAX_NODE_CNT; i++)
-	{
-		EPT(stderr, "%3d", i+1);
-	}
-	EPT(stderr, "\n");
 
-	for (i = 0; i < MAX_NODE_CNT; i++)
-	{
-		EPT(stderr, "%3d", i+1);
-		for (j = 0; j < MAX_NODE_CNT; j++)
-		{
-			EPT(stderr, "%3d", top[i][j].link);
-		}
-		EPT(stderr, "\n");
-	}
-}
-
-void show_top2()
+void mt_show_top()
 {
 	int i,j;
 	int vld = 0;
 
-	for (i = 0; i < MAX_NODE_CNT; i++)
+	for (i = 0; i < MAX_NODE_CNT; i++) 
 	{
 		vld = 0;
-		for (j = 0; j < MAX_NODE_CNT; j++)
+		for (j = 0; j < MAX_NODE_CNT; j++) 
 		{
-			if (0 == top[i][j].link)
+			if (0 == top[i][j])
 				continue;
-			vld = 1;
-			EPT(stderr, "t[%d][%d]=%d prob=%3.1f   ", i+1, j+1, top[i][j].link, top[i][j].relb);
+			vld = 1;			
+			EPT(stderr, "top[%d][%d]=%d  ", i+1, j+1, top[i][j]);
 		}
 		if (1 == vld)
 			EPT(stderr, "\n");
@@ -320,29 +299,4 @@ void show_rqs()
 	}
 }
 
-float mt_getlp()
-{
-	int r = rand();
-	return (float)r/RAND_MAX;
-}
-
-void  msg_copy(mmsg_t* dest, mmsg_t* src, int len)
-{
-	ASSERT(dest->mtype == MMSG_NULL);
-	ASSERT(src->mtype != MMSG_NULL);
-	ASSERT(len >= sizeof(dest->node));
-	dest->mtype = src->mtype;
-	dest->node = src->node;
-	memcpy(dest->data, src->data, len-sizeof(src->node));
-}
-
-__inline__ int wh_rp_msg(long type)
-{
-	return (MMSG_RPM == type)?1:0;
-}
-
-__inline__ int wh_mac_msg(long type)
-{
-	return (MMSG_MT_MDT == type)?1:0;
-}
 
